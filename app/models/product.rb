60081d61ea3884af97665ff1f6588a847f137f9d
Andrii Ageiev <a.ageyev@gmail.com>
2017-10-22 00:57:36 +0300

    Iteration E3: Finishing the Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text(65535)
+#  image_url   :string(255)
+#  price       :decimal(8, 2)
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Product < ApplicationRecord
   has_many :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
