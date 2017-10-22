47a6c3de1f44c53b416e693d309c72e364411129
Andrii Ageiev <a.ageyev@gmail.com>
2017-10-22 13:37:49 +0300

    Iteration G2: Atom Feeds

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -11,8 +11,9 @@
 #
 
 class LineItem < ApplicationRecord
-  belongs_to :product
   belongs_to :cart
+  belongs_to :product, optional: true
+  belongs_to :order, optional: true
 
   def total_price
     product.price * quantity
